2af2a71eea99b4d0a4d2a91eeb4f04670213b23c
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2023-05-28 18:52:19 -0300

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,7 +7,7 @@ export function setupAPIClient(ctx = undefined) {
     let cookies = parseCookies(ctx);
 
     const api = axios.create({
-        baseURL: 'http://localhost:3333',
+        baseURL: 'https://api-new-pz.vercel.app/',
         headers: {
             Authorization: `Bearer ${cookies['@nextauth.token']}`
         }
